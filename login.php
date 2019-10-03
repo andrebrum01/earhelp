df9b0a54e13c997e9e682355c88b9d1146490fcc
andrebrum01 <41603045+andrebrum01@users.noreply.github.com>
2019-10-03 16:43:21 -0400

    Add files via upload

--- login.php
+++ login.php
@@ -13,9 +13,7 @@ $result = mysqli_query($conexao, $query);
 $row = mysqli_num_rows($result);
 if($row == 1){
     $_SESSION['email']= $usuario;
-    echo $_SESSION['email'];
-    echo $query;
-    //header("location: painel.php");
+    header("location: painel.php");
 }
 else{
 	$_SESSION['nao_autenticado']=true;
