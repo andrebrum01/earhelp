0fef7c847da87f360c9b409899c079578ecbb24d
andrebrum01 <41603045+andrebrum01@users.noreply.github.com>
2019-10-04 00:56:00 -0400

    Add files via upload

--- painel.php
+++ painel.php
@@ -1,4 +1,5 @@
 <?php
+//include('login.php');
 session_start();
 echo $_SESSION['email'];
 ?>
@@ -11,7 +12,7 @@ echo $_SESSION['email'];
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <body>
 <h2>Bem Vindo, <?php echo $_SESSION['email'];?></h2>
-<h2><a href="/logout.php">SAIR</a></h2>
+<h2><a href="logout.php">SAIR</a></h2>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js">
     </script>
 <script type="text/javascript" src="js/actions.js"></script>
--- verificar_login.php
+++ verificar_login.php
@@ -1,6 +1,4 @@
 <?php
-print_r($_SESSION['email']);
-
 if(!$_SESSION['email']){
     header('Location: index.php');
     exit();
