9fe92de0a7310ff648aa98286b33f6184561a80b
andrebrum01 <41603045+andrebrum01@users.noreply.github.com>
2019-10-11 09:52:35 -0400

    Add files via upload

--- email.php
+++ email.php
@@ -17,9 +17,8 @@ if(mysqli_num_rows($result) == 1){
 	$senha = $usuario[1];
 	$usuario = $usuario[2];
 	$headers = "MIME-Version: 1.1\r\n";
-	$headers .= "Content-type: text/plain; charset=UTF-8\r\n";
+	$headers .= "Content-type: text/html; charset=UTF-8\r\n";
 	$headers .= "From: fmm.earhelp@gmail.com\r\n"; // remetente
-	$headers .= "Return-Path: fmm.earhelp@gmail.com\r\n"; // return-path
 	$envio=mail($email,"Esqueceu a senha do EAR HELP?","Caro <b>$usuario</b> sua senha é $senha",$headers);
 	if($envio)
 	 	echo "Mensagem enviada com sucesso";
