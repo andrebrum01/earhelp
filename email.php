3e7572bc8823691469c34f00e97ff696d5db1e8b
andrebrum01 <41603045+andrebrum01@users.noreply.github.com>
2019-10-11 11:44:05 -0400

    Add files via upload

--- email.php
+++ email.php
@@ -1,5 +1,7 @@
 <?php
 include("conexao.php");
+require_once("autoenvio/PHPMailerAutoload.php");
+
 $usuario = mysqli_real_escape_string($conexao,$_POST['email']);
 $query = "select email,senha,user from usuario where user='{$usuario}'";
 $result = mysqli_query($conexao, $query);
@@ -11,18 +13,32 @@ if(!mysqli_num_rows($result) == 1){
     	exit();
 	}
 }
-if(mysqli_num_rows($result) == 1){
-	$usuario=mysqli_fetch_row($result);
-	$email = $usuario[0];
-	$senha = $usuario[1];
-	$usuario = $usuario[2];
-	$headers = "MIME-Version: 1.1\r\n";
-	$headers .= "Content-type: text/html; charset=UTF-8\r\n";
-	$headers .= "From: fmm.earhelp@gmail.com\r\n"; // remetente
-	$envio=mail($email,"Esqueceu a senha do EAR HELP?","Caro $usuario sua senha é $senha",$headers);
-	if($envio)
-	 	echo "Mensagem enviada com sucesso";
-	else
-	 	echo "A mensagem não pode ser enviada";
-	}
+$usuario=mysqli_fetch_row($result);
+$email = $usuario[0];
+$senha = $usuario[1];
+$user = $usuario[2];
+$mail = new PHPMailer();
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->isHTML(true);
+$mail->Charset='UTF-8';
+$mail->SMTPAuth = true;
+$mail->SMTPSecure ='ssl';
+$mail-> Host='earhelp.cleverapps.io';
+$mail->Port=25;
+$mail->Username='fmm.earhelp@gmail.com';
+$mail->Password ='nescutoprr';
+$mail->From='fmm.earhelp@gmail.com';
+$mail->FromName=$user;
+$mail->Subject='Recuperar senha do Ear Help';
+$mail->Body="Sua senha é:$senha";
+$mail->AltBody="Sua senha é:$senha";
+$mail->AddAddress($email);
+if($mail->Send()){
+	echo "E-mail enviado com sucesso";
+}else{
+	echo "Erro no envio do e-mail: " . $mail->ErrorInfo;
+}
+
+
 ?>
