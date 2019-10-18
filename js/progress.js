11ab5ace3acc855f60abfc7d37801a170d4355d5
andrebrum01 <41603045+andrebrum01@users.noreply.github.com>
2019-10-18 17:57:44 -0400

    Add files via upload

--- js/progress.js
+++ js/progress.js
@@ -1,11 +1,23 @@
-var progress= $("#sons").children().length;
-for(var i=0;i<progress;i++){
-$("#caixaProgress").append("<span></span>");
-}
-$('.select').click(function() {
-    var index = $(".select").index();
-    $("#caixaProgress").children().eq(index).removeClass('select');
-    index++;
-    if(index==progress) index=0;
-    $("#caixaProgress").children().eq(index).addClass('select');
-});
+// declarando variaveis
+	var progress= $("#sons").children().length;
+	var index = $(".select").index();
+	var criancas = $("#sons");
+// add as bolinhas
+	for(var i=0;i<progress;i++){
+	$("#caixaProgress").append("<span></span>");
+	}
+	$("#caixaProgress").children().eq(0).addClass('select');
+// add largura no coisa
+	$("#sons").css({
+		width:"calc(100%*"+progress+")"
+	});
+// on click
+	$(".ouviu").click(function(){
+	$("#caixaProgress").children().eq(index).removeClass('select');
+	index++;
+	if(index==progress) index=0;
+	$("#sons").css({
+		transform: "translateX(calc(calc(-100%/"+progress+")*"+index+"))"
+	});
+	$("#caixaProgress").children().eq(index).addClass('select');
+	});
--- painel.php
+++ painel.php
@@ -9,53 +9,57 @@ include("verificar_login.php");
 </head>
 <link rel="stylesheet" type="text/css" href="css/style2.css">
 <body>
+	<!-- navigation bar -->
 	<div class="nav">
 		<div class="nameUser">Bem Vindo, <?php echo $_GET['user'];?></div>
 		<div class="btsair"><a class="btsair" href="logout.php">SAIR</a></div>
 	</div>
-<section class="firstView">
-	<div class="caixa">
-		<div class="titulo">Bem-vindo ao Ear Help</div>
-		<div class="conteudo">Faça o teste para verificar sua audição.</div>
-		<div id ="teste"class="teste">
-			<input type="checkbox" id="next" class="next">
-			<input type="checkbox" id="close" class="close">
-			<section id="boas" class="boas">
-				<label for="close">
-					<div class="fecha"><img src="img/close.png"></img></div>
-				</label>
-				<div class="primeira">
-					<h1>Coloque seu fones de ouvidos</h1>
-					<div><img src="img/fone.png"></img>
-					</div>
-					<div class="sel">></div>
-				</div>
-			</section>
-
+	<!-- first view -->
+	<input type="checkbox" id="next">
+	<section class="firstView">
+		<div class="caixa">
+			<div class="titulo">Bem-vindo ao Ear Help</div>
+			<div class="conteudo">Faça o teste para verificar sua audição.</div>
 			<section id="btPlay" class="btPlay">
-				<label for="next">
-				<img src="img/play.png"></img>
-				</label>
-			</section>
-
-			<section id="teste1" class="teste1">
-				<label for="next">
-					<div class="back"><img src="img/back.png"></img></div>
-				</label>
-				<div id="sons">
-					<div class="cx blue"></div>
-					<div class="cx red"></div>
-					<div class="cx purple"></div>
-				</div>
-				<div id="caixaProgress">
-				</div>
-			</section>
+			<label for="next">
+			<img src="img/play.png"></img>
+			</label>
 		</div>
-	</div>
-</section>
+	</section>
+	<!-- boas praticas -->
+	<input type="checkbox" id="close">
+	<section class="boas">
+		<label for="close">
+			<div class="fecha"><img src="img/close.png"></img></div>
+		</label>
+		<div class="primeira">
+			<h1>Coloque seu fones de ouvidos</h1>
+			<div><img src="img/fone.png"></img>
+			</div>
+			<div class="sel">Seguinte ></div>
+		</div>
+	</section>
 
+	<!-- area de testes -->
+	<section class="teste1">
+		<label for="next">
+			<div class="back"><img src="img/back.png"></img></div>
+		</label>
+		<div id="sons">
+			<div id="blue">
+				<div class="ouviu">:)</div>
+			</div>
+			<div id="red">
+				<div class="ouviu">:)</div>
+			</div>
+			<div id="purple">
+				<div class="ouviu">:)</div>
+			</div>
+		</div>
+		<div id="caixaProgress"/>
+	</section>
+	<!-- jquery and Js -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script type="text/javascript" src="js/progress.js"></script>
 </body>
-
 </html>
